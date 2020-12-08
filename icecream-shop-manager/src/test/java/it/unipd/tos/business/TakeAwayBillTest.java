3d8561bc5218a2d2307f5c4ceb9a1c0c2853bc2b
Davide Sut <davide.sut@studenti.unipd.it>
2020-12-08 13:55:49 +0100

    Aggiunti test per la issue 1

---
+++ icecream-shop-manager/src/test/java/it/unipd/tos/business/TakeAwayBillTest.java
@@ -0,0 +1,61 @@
+////////////////////////////////////////////////////////////////////
+// Davide Sut 1201267
+////////////////////////////////////////////////////////////////////
+
+
+package it.unipd.tos.business;
+
+import it.unipd.tos.business.exception.RestaurantBillException;
+import it.unipd.tos.model.MenuItem;
+import it.unipd.tos.model.User;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.LinkedList;
+
+import static org.junit.Assert.assertEquals;
+
+public class TakeAwayBillTest {
+
+    private TakeAwayBill takeAwayBill;
+    private MenuItem bananaSplit;
+    private MenuItem coppaNafta;
+    private MenuItem biancaneve;
+    private MenuItem pinguino;
+    private MenuItem chinotto;
+
+    LinkedList<MenuItem> list = new LinkedList<>();
+    private User user;
+
+    @Before
+    public void setup() {
+
+        bananaSplit = new MenuItem(MenuItem.ItemType.GELATO, "Banana Split", 3D);
+        coppaNafta = new MenuItem(MenuItem.ItemType.GELATO, "Coppa Nafta", 3.5D);
+        biancaneve = new MenuItem(MenuItem.ItemType.BUDINO, "Biancaneve", 6D);
+        pinguino = new MenuItem(MenuItem.ItemType.BUDINO, "Pinguino", 7D);
+        chinotto = new MenuItem(MenuItem.ItemType.BEVANDA, "Chinotto", 2.5D);
+
+        takeAwayBill = new TakeAwayBillImpl();
+        list = new LinkedList<>();
+        user = new User("user",21);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void test_getOrderPriceNullItem() throws RestaurantBillException {
+        takeAwayBill.getOrderPrice(null,user);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void test_getOrderPriceZeroItem() throws RestaurantBillException {
+        takeAwayBill.getOrderPrice(list,user);
+    }
+
+    @Test
+    public void test_getOrderPriceApplied() throws RestaurantBillException {
+        list.add(bananaSplit);
+        list.add(bananaSplit);
+        assertEquals(6D, takeAwayBill.getOrderPrice(list,user), 0.0001D);
+    }
+
+}
---
+++ icecream-shop-manager/src/test/java/it/unipd/tos/model/MenuItemTest.java
@@ -0,0 +1,56 @@
+////////////////////////////////////////////////////////////////////
+// Davide Sut 1201267
+////////////////////////////////////////////////////////////////////
+
+
+package it.unipd.tos.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MenuItemTest {
+
+    private MenuItem item;
+
+    @Before
+    public void setup() {
+        item = new MenuItem(MenuItem.ItemType.BEVANDA, "Chinotto", 3D);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void test_constructorNullItemTypeParam() {
+        new MenuItem(null, "Name", 8D);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void test_constructorNullNameParam() {
+        new MenuItem(MenuItem.ItemType.BEVANDA, null, 3D);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void test_constructorZeroLengthNameParam() {
+        new MenuItem(MenuItem.ItemType.BEVANDA, "", 3D);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void test_constructorNegativePriceParam() {
+        new MenuItem(MenuItem.ItemType.BEVANDA, "Fanta", -3D);
+    }
+
+    @Test
+    public void testNameGetter() {
+        assertEquals("Chinotto", item.getName());
+    }
+
+    @Test
+    public void testPriceGetter() {
+        assertEquals(3D, item.getPrice(), 0.0001D);
+    }
+
+    @Test
+    public void testTypeGetter() {
+        assertEquals(MenuItem.ItemType.BEVANDA, item.getItemType());
+    }
+}
